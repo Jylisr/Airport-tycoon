4f29d31b92bbb5795de1a6854a8965d15984be1e
Juho <juho.ylisir@gmail.com>
2023-10-03 15:01:52 +0300

    mysql.connector

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 '''Airport Tycoon
 
 By: Juho, Amaan, German, Chedel, Nhi
+
+Dependencies: mysql.connector
 '''
 
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='',
+    user='',
+    password='',
+)
+
+def startgame:
