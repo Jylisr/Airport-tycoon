e0effcc8dbde269a011c4d91560a2ccad58c1bc0
Amaan <amaan.brs@gmail.com>
2023-10-04 17:20:37 +0300

    4/10/2023

--- main.py
+++ main.py
@@ -13,10 +13,42 @@ connection = mysql.connector.connect(
     database='',
     user='',
     password='',
+    autocommit='True'
 )
+cursor=connection.cursor()
 
-def startgame:
+
+# copy paste function below to the database file
+
+def fetch_players():
+    cursor.execute("select * from goal;")
+    highscore_list=cursor.fetchall()
+    return highscore_list
+
+def start_game():
     print("Welcome to Airport Tycoon!")
-    print("[]")
+    print("You have now entered the wonderful world of airportopia.")
+    print("I see the spirit of a future tycoon in you.")
+    print("What is your name?")
+    name=int(input(""))
+    return name
+def name_to_table(name):
+    query = f"insert into goal(screen_name) values ('{name}');"
+    cursor.execute(query)
+    #goal table changes need to be kept in mind
+def name_check(name,highscore_list):
+    for i in highscore_list:
+        if i[0]==name:
+            print("Looks like you've already attempted the tycoon life")
+            break
+        else:
+            print("Ah, So you are a rookie.")
+            print("Welcome again to this world")
+            name_to_table(name)
 
-def gameintro:
+def high_score(highscore_list):
+    print("So as you're new around here let me show you the people that have left their mark here already")
+    print("Name, Score, Time")
+    for i in range(5):
+        print(highscore_list[i])
+
