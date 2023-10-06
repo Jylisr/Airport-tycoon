fbad068ee6fa7a720439d39e1c1e2d4d2d3a457d
grem-newfield <novoger123@gmail.com>
2023-10-06 10:48:27 +0300

    database progress

---
+++ database.sql
@@ -0,0 +1,1 @@
+source "./flight_simulator_database_script.sql"
--- main.py
+++ main.py
@@ -1,44 +1,48 @@
-'''Airport Tycoon
+"""Airport Tycoon
 
 By: Juho, Amaan, German, Chedel, Nhi
 
 Dependencies: mysql.connector
-'''
+"""
 
 import mysql.connector
 
+from mysql.connector.types import RowType
+from typing import List
+
 connection = mysql.connector.connect(
-    host='127.0.0.1',
-    port=3306,
-    database='',
-    user='',
-    password='',
-    autocommit='True'
+    host="127.0.0.1", port=3306, database="", user="", password="", autocommit="True"
 )
-cursor=connection.cursor()
+cursor = connection.cursor()
 
 
 # copy paste function below to the database file
 
-def fetch_players():
+
+def fetch_players() -> List[RowType]:
     cursor.execute("select * from goal;")
-    highscore_list=cursor.fetchall()
+    highscore_list = cursor.fetchall()
     return highscore_list
 
-def start_game():
+
+def start_game() -> str:
     print("Welcome to Airport Tycoon!")
     print("You have now entered the wonderful world of airportopia.")
     print("I see the spirit of a future tycoon in you.")
     print("What is your name?")
-    name=int(input(""))
+    name = str(input(""))
     return name
+
+
 def name_to_table(name):
     query = f"insert into goal(screen_name) values ('{name}');"
     cursor.execute(query)
-    #goal table changes need to be kept in mind
-def name_check(name,highscore_list):
+    # goal table changes need to be kept in mind
+
+
+def name_check(name, highscore_list):
     for i in highscore_list:
-        if i[0]==name:
+        if i[1] == name:
             print("Looks like you've already attempted the tycoon life")
             break
         else:
@@ -46,14 +50,22 @@ def name_check(name,highscore_list):
             print("Welcome again to this world")
             name_to_table(name)
 
+
 def high_score(highscore_list):
-    print("So as you're new around here let me show you the people that have left their mark here already")
+    print(
+        "So as you're new around here let me show you the people that have left their mark here already"
+    )
     print("Name, Score, Time")
     for i in range(5):
         print(highscore_list[i])
 
-#main
-player_name = start_game() #game starts
-player_table = fetch_players() #players list is retrieved
-name_check(player_name, player_table) # name checked and name is added to database
-high_score(player_name) #high score is printed
+
+def main():
+    player_name = start_game()  # game starts
+    player_table = fetch_players()  # players list is retrieved
+    name_check(player_name, player_table)  # name checked and name is added to database
+    high_score(player_name)  # high score is printed
+
+
+if __name__ == "__main__":
+    main()
